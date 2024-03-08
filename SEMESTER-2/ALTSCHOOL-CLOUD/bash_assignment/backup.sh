fb08d45344a45b538d27f5ca32b510e1d5e86085
Olugbenga Alao <olugbengagt@gmail.com>
2024-03-08 11:04:29 +0100

    Wrapped up bash assignment

--- SEMESTER-2/ALTSCHOOL-CLOUD/bash_assignment/backup.sh
+++ SEMESTER-2/ALTSCHOOL-CLOUD/bash_assignment/backup.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/bash
 
-# Define source and destination directories
-    # source_dir="/path/to/source_directory"
-    # timestamp=$(date +'%Y%m%d%H%M%S')
-    # destination_tar="backup_${timestamp}.tar.gz"
-
-# Create tar archive with timestamp
-    # tar -czvf "$destination_tar" "$source_dir"
-
 if [[ -z $1 || -z $2 ]]; then
     echo "Either source or destination directory is missing"
 else
